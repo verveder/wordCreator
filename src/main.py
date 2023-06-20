6ebd92eedb3588c76261463a96f32d112f75fc0c
Veronika Vedernikova <129505928+verveder@users.noreply.github.com>
2023-06-20 23:29:11 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+""" 
+
+This module is designed to be main some day 
+still WorkInProgress
+
+""""
+
+
+def run_creator():
+    print('app started')
+
+
+if __name__ == "__main__":
+    run_creator()
