196d2346db7917de93ce7ca1dfa1b4111d710a86
Veronika Vedernikova <129505928+verveder@users.noreply.github.com>
2023-06-20 23:27:00 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -24,7 +24,7 @@ COLOR_BG = '#D3B890'
 COLOR_FIELD = '#EDDECC'
 
 assets_path = Path(
-    r'C:\Users\Veronica\PycharmProjects\wordCreator\src\build\assets\images'
+    r'[YOUR PATH THERE]\wordCreator\src\build\assets\images'
     )
 
 default_output = 'Line below is 34 "✦" symbols,' \
