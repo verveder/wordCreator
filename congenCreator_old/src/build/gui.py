a4ea1464ef4c7212f7eecfce190792ca20774d5a
Veronika Vedernikova <129505928+verveder@users.noreply.github.com>
2023-06-21 00:18:07 +0300

    Update gui.py

--- congenCreator_old/src/build/gui.py
+++ congenCreator_old/src/build/gui.py
@@ -13,7 +13,7 @@ import src.congen as cg
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(
-    r"[YOUR DIRECTORY HERE]\con_generator\src\build\assets\frame0"
+    r"[YOUR DIRECTORY HERE]\src\build\assets\frame0"
 )
 
 
